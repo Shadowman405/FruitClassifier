d5bbd8fa1c94a62d043228ca3f3cadeaba49ba48
Maxim Mitin <maximus405@gmail.com>
2021-11-21 22:01:14 +0300

    Initial Commit

---
+++ FruitClassifier/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FruitClassifier
+//
+//  Created by Maxim Mitin on 21.11.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
